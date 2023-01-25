e912f3acd79f565cbb1fb2fd96c0d8c6a9f6fd01
Uwem Akpabot <akpabot.it@gmail.com>
2023-01-25 22:53:25 +0100

    backend login register added

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify, request
+import json
+from flask import Flask, jsonify, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 import datetime
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +20,7 @@ class Articles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     body = db.Column(db.Text())
-    date = db.Column(db.DateTime, default=datetime.datetime.now)
+    date = db.Column(db.DateTime, default=datetime.now)
 
     def __init__(self, title, body):
         self.title = title
@@ -25,6 +28,75 @@ class Articles(db.Model):
 
 app.app_context().push()
 
+app.config["JWT_SECRET_KEY"] = "a__psalexggase-res()`-Khshmemb4y37jn_!me"
+jwt = JWTManager(app)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+    
+@app.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token":access_token}
+    return response
+
+@app.route('/profile')
+@jwt_required()
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+# REGISTER
+# @app.route('/register', methods=['POST'])
+# def register_user():
+#     email = request.json['email']
+#     password = request.json['password']
+
+#     user_exists = User.query.filter_by(email=email).first() is not None
+
+#     if user_exists:
+#         abort(409)
+
+#     hashed_password = bcrypt.generate_password_hash(password)
+#     new_user = User(email=email, password=hashed_password)
+
+#     db.session.add(new_user)
+#     db.session.commit()
+#     return jsonify({
+#         "id": new_user.id,
+#         "email": new_user.email
+#     })
+ 
 class ArticleSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'body', 'date')
